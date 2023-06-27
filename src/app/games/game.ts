eaa8d3354c38bcd4206b9ac3316745def719b388
Keith Ott <ducttape12@yahoo.com>
2023-06-27 16:09:24 -0500

    Games uses localStorage

--- src/app/games/game.ts
+++ src/app/games/game.ts
@@ -1,8 +1,10 @@
 export class Game {
-    title:string;
-    platform:string;
+    id: number;
+    title: string;
+    platform: string;
 
-    constructor(title:string, platform:string) {
+    constructor(id: number, title: string, platform: string) {
+        this.id = id;
         this.title = title;
         this.platform = platform;
     }
--- src/app/games/games.service.ts
+++ src/app/games/games.service.ts
@@ -6,12 +6,73 @@ import { Game } from './game';
 })
 export class GamesService {
 
-  constructor() { }
+  private static readonly GamesLocalStorageKey = 'games';
+
+  games: Game[];
+  nextId: number;
+
+  constructor() {
+    this.games = this.loadGames();
+
+    let id = 0;
+    for (let game of this.games) {
+      if (game.id > id) {
+        id = game.id;
+      }
+    }
+
+    this.nextId = id + 1;
+  }
 
   getGames(): Game[] {
-    return [
-      new Game('Ms. Pac-Man', 'Game Boy'),
-      new Game('Tetris', 'Game Boy')
-    ];
+    return this.games;
+  }
+
+  private loadGames(): Game[] {
+    const rawGames = localStorage.getItem(GamesService.GamesLocalStorageKey);
+    if (rawGames === null) {
+      return [];
+    }
+
+    try {
+      return JSON.parse(rawGames);
+    } catch (e) {
+      return [];
+    }
+  }
+
+  private saveGames() {
+    const jsonGames = JSON.stringify(this.games);
+    localStorage.setItem(GamesService.GamesLocalStorageKey, jsonGames);
+  }
+
+  private sortGames() {
+    this.games.sort((a, b) => {
+      if (a.title < b.title) {
+        return -1;
+      }
+
+      if (a.title === b.title) {
+        return 0;
+      }
+
+      return 1;
+    });
+  }
+
+  addGame(newGame: Game) {
+    newGame.id = this.nextId;
+    this.games.push(newGame);
+    this.nextId++;
+
+    this.sortGames();
+    this.saveGames();
+  }
+
+  deleteGame(gameId: number) {
+    this.games = this.games.filter(g => g.id != gameId);
+
+    this.sortGames();
+    this.saveGames();
   }
 }
